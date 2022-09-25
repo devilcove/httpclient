0170ce54a33fd78672af702b303e7e323cd9c488
Matthew R. Kasun <mkasun@nusak.ca>
2022-09-25 13:29:35 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nusak/httpclient
+
+go 1.19
---
+++ httpclient.go
@@ -0,0 +1,58 @@
+package httpclient
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+var Client http.Client
+
+func init() {
+	Client := http.Client{
+		TimeOut: 30 * time.Second,
+	}
+}
+
+func API(data, resp any, method, url, auth string) (any, error) {
+	var request *http.Request
+	var err error
+	if data != "" {
+		payload, err := json.Marshal(data)
+		if err != nil {
+			return request, fmt.Errorf("error encoding data %w", err)
+		}
+		request, err = http.NewRequest(method, url, bytes.NewBuffer(payload))
+		if err != nil {
+			return request, fmt.Errorf("error creating http request %w", err)
+		}
+		request.Header.Set("Content-Type", "application/json")
+	} else {
+		request, err = http.NewRequest(method, url, nil)
+		if err != nil {
+			return request, fmt.Errorf("error creating http request %w", err)
+		}
+	}
+	if auth != "" {
+		request.Header.Set("Authorization", "Bearer "+auth)
+	}
+	response, err := Client.Do(request)
+	if err != nil {
+		return response, err
+	}
+	defer response.Body.Close()
+	data, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != http.StatusOK {
+		return data, fmt.Errorf("endpoint return status %s", resp.Status)
+	}
+	if err = json.Unmarshal(data, resp); err != nil {
+		return nil, fmt.Errorf("response not json %w", err)
+	}
+	return resp, nil
+}
