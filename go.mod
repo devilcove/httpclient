ba2443c3dbc71cd92452acb1e63710516a16a1ca
Matthew R. Kasun <mkasun@nusak.ca>
2022-09-27 14:54:55 -0400

    return http.response

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module gitlab.com/nusak/httpclient
+module github.com/devilcove/httpclient
 
-go 1.19
+go 1.18
--- httpclient.go
+++ httpclient.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"time"
 )
@@ -17,42 +16,28 @@ func init() {
 	}
 }
 
-func API(data, resp any, method, url, auth string) (any, error) {
+func API(data, resp any, method, url, auth string) (*http.Response, error) {
 	var request *http.Request
+	var response *http.Response
 	var err error
 	if data != "" {
 		payload, err := json.Marshal(data)
 		if err != nil {
-			return request, fmt.Errorf("error encoding data %w", err)
+			return response, fmt.Errorf("error encoding data %w", err)
 		}
 		request, err = http.NewRequest(method, url, bytes.NewBuffer(payload))
 		if err != nil {
-			return request, fmt.Errorf("error creating http request %w", err)
+			return response, fmt.Errorf("error creating http request %w", err)
 		}
 		request.Header.Set("Content-Type", "application/json")
 	} else {
 		request, err = http.NewRequest(method, url, nil)
 		if err != nil {
-			return request, fmt.Errorf("error creating http request %w", err)
+			return response, fmt.Errorf("error creating http request %w", err)
 		}
 	}
 	if auth != "" {
 		request.Header.Set("Authorization", "Bearer "+auth)
 	}
-	response, err := Client.Do(request)
-	if err != nil {
-		return response, err
-	}
-	defer response.Body.Close()
-	data, err = io.ReadAll(response.Body)
-	if err != nil {
-		return nil, err
-	}
-	if response.StatusCode != http.StatusOK {
-		return data, fmt.Errorf("endpoint return status %s", response.Status)
-	}
-	if err = json.Unmarshal(data.([]byte), resp); err != nil {
-		return nil, fmt.Errorf("response not json %w", err)
-	}
-	return resp, nil
+	return Client.Do(request)
 }
